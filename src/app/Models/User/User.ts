4f1021d2bfd914478afdbdd3926895984d328d7a
luizaontc <lhenriquesoaresdasilva@gmail.com>
2023-05-29 00:23:14 -0300

    First Commit front

---
+++ src/app/Models/User/User.ts
@@ -0,0 +1,16 @@
+export class User{
+    id: number;
+    username:string;
+    password:string;
+    document:string;
+    userImage:string;
+    Email:string;
+    Deleted:boolean;
+    DeletedDate: Date;
+    UserDeletedId:number;
+    CreationDate: Date;
+    UserCreationId:number;
+    ModifiedDate: Date;
+    UserModifiedId:number;
+    Name:string;
+}
